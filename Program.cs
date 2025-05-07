6ca1ed917bb7165c0df8cfa9f8cf4f549ec9212e
Luis <luisfelipe782006@gmail.com>
2025-05-07 10:32:45 -0300

    Configurar logs con Serilog y ajustes en configuración
    - Se añadieron las dependencias `Serilog.Settings.Configuration` y
      `Serilog.Sinks.File` en `HotelTools.csproj`.
    - Configuración de logs en `Program.cs` con Serilog, incluyendo
      rotación diaria y formato específico.
    - Limpieza de namespaces no utilizados en `Program.cs`.
    - Configuración para leer variables de entorno y archivos JSON
      (`appsettings.json` y `appsettings.{Environment}.json`).
    - Comentadas configuraciones de autenticación, autorización y cookies.
    - Añadida sección `Util` con `ClaveSecreta` en `appsettings.json` y
      `appsettings.Development.json`.
    - Configuración de niveles mínimos de log en `appsettings.json`.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 using HotelTools.Components;
 //using HotelTools.Models;
-
-//using HotelTools.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
+using Serilog;
+using Serilog.Events;
+using MudBlazor.Charts;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,38 +16,61 @@ builder.Services.AddRazorComponents()
 //MudBlazor Services
 builder.Services.AddMudServices();
 
-//Agregar Servicio de Configuration
+// CONFIGURACION DE LECTURA DE VARIABLES DE ENTORNO
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile(
+        $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+        optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables()
     .Build();
 builder.Services.AddSingleton(configuration);
+//==============================================================
+
+// CONFIGURACION DE LOGS
+Directory.CreateDirectory("Logs"); // CREO LA CARPETA SINO EXISTE
+
+//Ademas de los Paquetes Serilog y Serilog.Sinks.File, se necesita el paquete Serilog.Settings.Configuration
+//dotnet add package Serilog.Settings.Configuration. 
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration) // Leer configuración desde appsettings.json
+    .Enrich.WithProperty("Application", "BlazorApp")
+    .Enrich.FromLogContext()
+    .WriteTo.File(
+        Path.Combine("Logs", "log-.txt"),
+        rollingInterval: RollingInterval.Day,
+        outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level}] {Message}{NewLine}{Exception}"
+    )
+    .CreateLogger();
+//===============================================================
+
 
 //Agrego servicio de cadena de conexion
 //builder.Services.AddDbContext<Hotel_ToolsContext>(options =>
-  // options.UseSqlServer(builder.Configuration.GetConnectionString("Hotel_Tools")));
+// options.UseSqlServer(builder.Configuration.GetConnectionString("Hotel_Tools")));
 
 //Agregar Servicio de Autenticación y Autorización
 //builder.Services.AddIdentity<Empleado,Rol>()
-  //  .AddDefaultTokenProviders()
-  //  .AddEntityFrameworkStores<Hotel_ToolsContext>();
+//  .AddDefaultTokenProviders()
+//  .AddEntityFrameworkStores<Hotel_ToolsContext>();
 
 //Configura Cookie Authentication
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
-    options.LoginPath = "/Account/Login";
-    options.AccessDeniedPath = "/Account/AccessDenied";
-    options.SlidingExpiration = true;
-});
+//builder.Services.ConfigureApplicationCookie(options =>
+//{
+//    options.Cookie.HttpOnly = true;
+//    options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+//    options.LoginPath = "/Account/Login";
+//    options.AccessDeniedPath = "/Account/AccessDenied";
+//    options.SlidingExpiration = true;
+//});
 
-//Politicas a
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
-    options.AddPolicy("RequireEmpleadoRole", policy => policy.RequireRole("Empleado"));
-});
+////Politicas a
+//builder.Services.AddAuthorization(options =>
+//{
+//    options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+//    options.AddPolicy("RequireEmpleadoRole", policy => policy.RequireRole("Empleado"));
+//});
 
 
 builder.Services.AddRazorComponents();
